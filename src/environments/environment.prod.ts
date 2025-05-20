f93cf2aa7158771593c7329d2bca678de5f0d49b
fbablu <fardeeneb@gmail.com>
2025-05-20 16:50:09 -0500

    fix: bigq api working again

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:5000/api',
+  apiUrl: 'https://predict-outage-358654363672.us-central1.run.app',
   google_maps_api: 'AIzaSyBX_SabtgpAEqy2JFox4ATWVKvVDnOEonM',
   open_weather_api: '94c47d44a519b8425ca6f7d5079cbce9',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,4 +8,5 @@ export const environment = {
   apiUrl: 'https://predict-outage-358654363672.us-central1.run.app',
   google_maps_api: 'AIzaSyBX_SabtgpAEqy2JFox4ATWVKvVDnOEonM',
   open_weather_api: '94c47d44a519b8425ca6f7d5079cbce9',
+  
 };
